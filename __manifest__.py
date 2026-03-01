5278c2afba66440dc601068df8ee1ee796811e88
pmorettobr <pmorettobr@gmail.com>
2026-03-01 19:56:37 -0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Repair Quality Simple',
+    'version': '16.0.1.0',
+    'summary': 'Checklist simples de CQ para Repair Orders',
+    'author': 'Paulo Moretto',
+    'depends': ['repair', 'hr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/repair_quality_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
